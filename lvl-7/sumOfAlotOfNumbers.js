59a2f64beaefa4f4cf0840259e7819dfc6cb561a
Resilient Coders <resilientcoders@Resilients-MacBook-Air.local>
2021-10-06 22:03:59 -0400

    codewars solution

---
+++ lvl-7/sumOfAlotOfNumbers.js
@@ -0,0 +1,11 @@
+// Your task is to help the young 
+// Carl Friedrich to solve this 
+// problem as quickly as you can; 
+// so, he can astonish his teacher 
+// and rescue his recreation interval.
+
+
+
+function f(n){
+    return (parseInt(n) === n && n > 0) ? n*(n+1)/2 : false;
+  };
