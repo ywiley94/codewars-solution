49c65c8061760a8fed61ce755020292766a94308
Yannick Wiley <yannick.wiley@gmail.com>
2021-10-20 19:13:57 -0400

    codewars soltution

---
+++ lvl-8/countingSheep.js
@@ -0,0 +1,23 @@
+// Consider an array/list of sheep 
+// where some sheep may be missing 
+// from their place. We need a function 
+// that counts the number of sheep present 
+// in the array (true means present).
+
+
+function countSheeps(arrayOfSheep) {
+    let sum = 0
+    // TODO May the force be with you
+    for(let i=0; i < arrayOfSheep.length; i++) {
+      if(arrayOfSheep[i]) {
+        sum += 1
+      }
+    }
+    return sum
+  }
+  countSheeps([true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true])
