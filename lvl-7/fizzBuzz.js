f0bf2c13facae98d8c12d0beaca0982e055ccc6a
Yannick Wiley <yannick.wiley@gmail.com>
2021-12-27 15:20:08 -0500

    codewars solution

---
+++ lvl-7/fizzBuzz.js
@@ -0,0 +1,30 @@
+// Return an array containing the numbers from 1 to N, 
+// where N is the parametered value.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value "Fizz" instead
+// If the value is a multiple of 5: use the value "Buzz" instead
+// If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+// N will never be less than 1.
+
+// Method calling example:
+
+// Return an array
+function fizzbuzz(n)
+{
+  let newArr = []
+  //
+  for(var i=1; i<=n; i++) {
+    if(i % 3 === 0) {
+      newArr.push('Fizz')
+    } else if(i % 5 === 0) {
+      newArr.push('Buzz')
+    } else if(i % 15 === 0) {
+      newArr.push('FizzBuzz')
+    } else{
+      newArr.push(i)
+    }
+  }
+  return newArr
+}
