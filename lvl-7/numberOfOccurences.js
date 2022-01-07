955a9135f6d494fb6327c39a6c7cc727fca08163
Yannick Wiley <yannick.wiley@gmail.com>
2022-01-06 23:30:06 -0500

    codewars solution

---
+++ lvl-7/numberOfOccurences.js
@@ -0,0 +1,18 @@
+// Write a function that returns the number of occurrences of an element in an array.
+
+// Examples
+// var arr = [0, 1, 2, 2, 3];
+// arr.numberOfOccurrences(0) === 1;
+// arr.numberOfOccurrences(4) === 0;
+// arr.numberOfOccurrences(2) === 2;
+// arr.numberOfOccurrences("a") === 0;
+
+Array.prototype.numberOfOccurrences = function(search) {
+    var counter = 0
+    for(var i=0; i < this.length; i++) {
+      if(this[i] == search) {
+        counter++
+      }
+    }
+    return counter
+  }
